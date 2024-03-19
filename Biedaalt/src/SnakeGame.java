5bc2e9f77c60dfaef9667222db466da68b7e27b8
Irmuun1214 <125433652+Irmuun1214@users.noreply.github.com>
2024-03-19 18:22:15 +0800

    Update SnakeGame.java

--- Biedaalt/src/SnakeGame.java
+++ Biedaalt/src/SnakeGame.java
@@ -1,5 +1,3 @@
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,22 +6,24 @@ import java.awt.event.ActionListener;
 public class SnakeGame {
     public static void main(String[] args) {
         int boardWidth = 600;
-        int boardHeight = boardWidth;
+        int boardHeight = 600;
 
         // Create the JFrame
         JFrame frame = new JFrame("Snake Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit the application when the frame is closed
-        frame.setSize(boardWidth, boardHeight); // Set the size of the frame
-        frame.setResizable(false); // Disable frame resizing
+        frame.setSize(boardWidth+25, boardHeight+75); // Set the size of the frame
+        frame.setResizable(true); // Disable frame resizing
         frame.setLocationRelativeTo(null); // Center the frame on the screen
 
         // Create the game panel
-        GamePanel snakeGame = new GamePanel(boardWidth, boardHeight);
+        GamePanel snakeGame = new GamePanel(boardWidth, boardHeight, 2);
 
         // Create a control panel with play and restart buttons
         JPanel controlPanel = new JPanel();
         JButton playButton = new JButton("Play");
         JButton restartButton = new JButton("Restart");
+        JButton singlePlayerButton = new JButton("1 Player");
+        JButton twoPlayerButton = new JButton("2 Players");
 
         // Add action listeners to the buttons
         playButton.addActionListener(new ActionListener() {
@@ -41,9 +41,27 @@ public class SnakeGame {
                 snakeGame.requestFocus();
             }
         });
+        singlePlayerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                snakeGame.setNumPlayers(1);
+                snakeGame.restartGame();
+                snakeGame.requestFocus();
+            }
+        });
+        twoPlayerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                snakeGame.setNumPlayers(2);
+                snakeGame.restartGame();
+                snakeGame.requestFocus();
+            }
+        });
 
         controlPanel.add(playButton);
         controlPanel.add(restartButton);
+        controlPanel.add(singlePlayerButton);
+        controlPanel.add(twoPlayerButton);
 
         // Add components to the frame
         frame.add(snakeGame, BorderLayout.CENTER);
